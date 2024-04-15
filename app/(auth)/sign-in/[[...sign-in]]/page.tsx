845b0942a2b9d56498e124dad75b25741f2fa26d
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-04-15 13:48:33 -0700

    rework auth page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,25 @@
+// import React from 'react'
+// import {SignIn} from '@clerk/nextjs'
+
+// const SignInPage = () => {
+//   return (
+//     <div className='sign-in'>
+    
+//     <div><p style={{color: "rgba(0, 0, 0, 0.65)"}}>Welcome to</p><h1 className='h1-semibold'>EZEdit</h1></div>
+//     <p style={{marginBottom: "32px",color: "rgba(0, 0, 0, 0.65)"}}>Please sign up to begin FREE AI image manipulation</p>
+//     <SignIn/>
+//   </div>
+//   )
+// }
+
+// export default SignInPage
+
 import React from 'react'
 import {SignIn} from '@clerk/nextjs'
 
 const SignInPage = () => {
-  return (
-    <div className='sign-in'>
-    
-    <div><p style={{color: "rgba(0, 0, 0, 0.65)"}}>Welcome to</p><h1 className='h1-semibold'>EZEdit</h1></div>
-    <p style={{marginBottom: "32px",color: "rgba(0, 0, 0, 0.65)"}}>Please sign up to begin FREE AI image manipulation</p>
-    <SignIn/>
-  </div>
-  )
+  return <SignIn/>
+
 }
 
 export default SignInPage
