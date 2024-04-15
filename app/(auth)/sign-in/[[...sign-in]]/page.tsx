38e2402a8dc8e34c7ca0be4a16f4520221e4de5f
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-04-15 10:21:30 -0700

    auth page text

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import {SignIn} from '@clerk/nextjs'
 
 const SignInPage = () => {
-  return <SignIn/>
+  return (
+    <div className='sign-in'>
+    
+    <div><p>Welcome to</p><h1 className='h1-semibold'>EZEdit</h1></div>
+    <p style={{marginBottom: "32px"}}>Please sign up to begin FREE AI image manipulation</p>
+    <SignIn/>
+  </div>
+  )
 }
 
 export default SignInPage
