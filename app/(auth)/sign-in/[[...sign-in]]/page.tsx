633d0bf7cd5a0ec4dd28e60d9e5a62f7baf6a7c6
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-04-15 10:24:01 -0700

    color text on auth page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,8 +5,8 @@ const SignInPage = () => {
   return (
     <div className='sign-in'>
     
-    <div><p>Welcome to</p><h1 className='h1-semibold'>EZEdit</h1></div>
-    <p style={{marginBottom: "32px"}}>Please sign up to begin FREE AI image manipulation</p>
+    <div><p style={{color: "rgba(0, 0, 0, 0.65)"}}>Welcome to</p><h1 className='h1-semibold'>EZEdit</h1></div>
+    <p style={{marginBottom: "32px",color: "rgba(0, 0, 0, 0.65)"}}>Please sign up to begin FREE AI image manipulation</p>
     <SignIn/>
   </div>
   )
